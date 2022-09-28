e5dbbfd3c8422fe63bf0db2df3291aeeb551d4b4
Krawist <krawist@savics.org>
2022-09-28 18:54:35 +0200

    QALI-272 Create scenario examples for MongoDB

---
+++ mongodb/src/main/kotlin/io/qalipsis/examples/mongodb/MongoDbSaveAndSearch.kt
@@ -0,0 +1,113 @@
+package io.qalipsis.examples.mongodb
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.mongodb.reactivestreams.client.MongoClients
+import io.kotest.assertions.asClue
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.ints.shouldBeExactly
+import io.qalipsis.api.annotations.Scenario
+import io.qalipsis.api.executionprofile.regular
+import io.qalipsis.api.scenario.scenario
+import io.qalipsis.api.steps.map
+import io.qalipsis.api.steps.verify
+import io.qalipsis.examples.utils.BatteryState
+import io.qalipsis.examples.utils.BatteryStateContract
+import io.qalipsis.examples.utils.DatabaseConfiguration
+import io.qalipsis.examples.utils.DatabaseConfiguration.Companion.NUMBER_MINION
+import io.qalipsis.plugins.jackson.csv.csvToObject
+import io.qalipsis.plugins.jackson.jackson
+import io.qalipsis.plugins.mongodb.mongodb
+import io.qalipsis.plugins.mongodb.save.save
+import io.qalipsis.plugins.mongodb.search.search
+import org.bson.Document
+
+class MongoDbSaveAndSearch {
+
+    private val objectMapper = ObjectMapper().also {
+        it.registerModule(JavaTimeModule())
+    }
+
+    @Scenario("mongodb-save-and-search")
+    fun scenarioSaveAndSearch() {
+        //we define the scenario, set the name, number of minions and rampUp
+        scenario {
+            minionsCount = NUMBER_MINION
+            profile {
+                regular(periodMs = 1000, minionsCountProLaunch = minionsCount)
+            }
+        }
+            .start()
+            .jackson() //we start the jackson step to fetch data from the csv file. we will use the csvToObject method to map csv entries to list of utils.BatteryState object
+            .csvToObject(BatteryState::class) {
+
+                classpath("battery-levels.csv")
+                // we define the header of the csv file
+                header {
+                    column("deviceId")
+                    column("timestamp")
+                    column("batteryLevel").integer()
+                }
+                unicast()
+            }
+            .map { it.value } // we transform the output of the CSV reader entries to utils.BatteryState
+            .mongodb()// we start the mongodb step to save data in mongodb database
+            .save {
+
+                //setup connection of the database
+                connect {
+                    MongoClients.create(DatabaseConfiguration.SERVER_LINK)
+                }
+
+                query {
+                    database { _, _ ->
+                        DatabaseConfiguration.DATABASE
+                    }
+
+                    collection { _, _ ->
+                        DatabaseConfiguration.COLLECTION
+                    }
+
+                    documents { _, input ->
+                        listOf(Document.parse(objectMapper.writeValueAsString(input)))
+                    }
+                }
+            }
+            .map { it.input }
+            .mongodb()
+            .search {
+                name = "search"
+
+                connect {
+                    MongoClients.create(DatabaseConfiguration.SERVER_LINK)
+                }
+
+                search {
+                    database { _, _ -> DatabaseConfiguration.DATABASE }
+                    collection { _, _ -> DatabaseConfiguration.COLLECTION }
+                    query { _, input ->
+                        Document(
+                            mapOf(
+                                BatteryStateContract.DEVICE_ID to input.deviceId,
+                                BatteryStateContract.TIMESTAMP to input.timestamp.epochSecond
+                            )
+                        )
+                    }
+                }
+            }
+            .map {
+                it.input to it.documents.map { document ->
+                    objectMapper.readValue(document.toJson(), BatteryState::class.java)
+                }
+            }
+            .verify { result ->
+                result.asClue {
+                    assertSoftly {
+                        it.second.size shouldBeExactly 1
+                        it.first.batteryLevel shouldBeExactly it.second.first().batteryLevel
+                    }
+                }
+            }
+    }
+
+}
